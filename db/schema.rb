8dfcd07131162048c39d85630d148a5b0ad4d3d3
tahina67 <tahinarakotoniain@gmail.com>
2020-12-15 12:50:37 +0000

    first push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_085900) do
+ActiveRecord::Schema.define(version: 2020_08_25_082203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -290,6 +290,7 @@ ActiveRecord::Schema.define(version: 2020_06_17_085900) do
     t.string "ville"
     t.string "pays"
     t.string "confirm_token"
+    t.string "statut", default: "actif"
   end
 
   create_table "products", force: :cascade do |t|
