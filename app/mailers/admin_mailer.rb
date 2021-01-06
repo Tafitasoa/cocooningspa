9c592a0b3b8de8276a0c8a5d2822fc462de46728
Cédric Maminjanahary <tafitasoa.cedric@gmail.com>
2021-01-06 15:47:02 +0300

    Change admin receiver mail to Alex, for test on Heroku

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -8,7 +8,7 @@ class AdminMailer < ApplicationMailer
 
         #mail(to: 'contact@cocooningspa.com', subject: "Un client a besoin d'information ou d'aide")
 
-        mail(to: 'michael77rakotovao@gmail.com', subject: "Un client a besoin d'information ou d'aide")
+        mail(to: 'k.randriam@outlook.fr', subject: "Un client a besoin d'information ou d'aide")
     end
 
     def new_partner(candidate)
@@ -21,31 +21,31 @@ class AdminMailer < ApplicationMailer
 
         #mail(to: 'contact@cocooningspa.com', subject: '​Nouvelle demande de partenariat !')
 
-        mail(to: 'michael77rakotovao@gmail.com', subject: '​Nouvelle demande de partenariat !')
+        mail(to: 'k.randriam@outlook.fr', subject: '​Nouvelle demande de partenariat !')
     end
 
     def new_order_relance_client(order_id,client_id)
         @client = Client.find(client_id)
-    @order = Order.find(order_id)
-    @date = @order.prestation_date
-    @heurs = ["",""]
-    @number_service = 0
-    @order.order_services.each do |o_s|
-      @number_service += 1
-        if o_s.service.name == "Location spa"
-            @heurs[0] = o_s.service_time
-        elsif o_s.service.name == "Massage"
-            @heurs[1] = o_s.service_time
+        @order = Order.find(order_id)
+        @date = @order.prestation_date
+        @heurs = ["",""]
+        @number_service = 0
+        @order.order_services.each do |o_s|
+        @number_service += 1
+            if o_s.service.name == "Location spa"
+                @heurs[0] = o_s.service_time
+            elsif o_s.service.name == "Massage"
+                @heurs[1] = o_s.service_time
+            end
         end
-    end
-    @code_promo = 0
-    if @order.code_promo
-      @code_promo = @order.code_promo.reduction
-    end
-    #mail(to: 'cocooningspa@outlook.com', subject: 'Votre devis effectué en ligne !')
+        @code_promo = 0
+        if @order.code_promo
+        @code_promo = @order.code_promo.reduction
+        end
+        #mail(to: 'cocooningspa@outlook.com', subject: 'Votre devis effectué en ligne !')
 
-    #mail(to: 'contact@cocooningspa.com', subject: 'Votre devis effectué en ligne !')
-    mail(to: 'michael77rakotovao@gmail.com', subject: 'Votre devis effectué en ligne !')
+        #mail(to: 'contact@cocooningspa.com', subject: 'Votre devis effectué en ligne !')
+        mail(to: 'k.randriam@outlook.fr', subject: 'Votre devis effectué en ligne !')
     end
 
 end
